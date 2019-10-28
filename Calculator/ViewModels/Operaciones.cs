2c227fb68775fd925fa58a690549ccf209796240
ManuelPanduro18 <edson.panduro@tecsup.edu.pe>
2019-10-28 15:41:14 -0500

    Subiendo lo avanzado de la App

---
+++ Calculator/ViewModels/Operaciones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator.ViewModels
+{
+    public class Operaciones
+    {
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+        private Button sender;
+
+        void OnSelectNumber(string result)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            if (result == "0" || currentState < 0)
+            {
+                result = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            result += pressed;
+
+            double number;
+            if (double.TryParse(result, out number))
+            {
+                result = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# 5to_Ciclo_Semana11_ApliMovAvan_Practica3
+---
+name: Xamarin.Forms - Basic Calculator
+description: "A basic calculator app for Android and iOS using Xamarin.Forms"
+page_type: sample
+languages:
+- csharp
+products:
+- xamarin
+urlFragment: liveplayer-basiccalculator
+---
+# Xamarin.Forms - Basic Calculator
+
+This Xamarin.Forms app provides a simple calculator you can use on your Android or iOS device:
+
+![Calculator app](Screenshots/basic-calculator-sml.png)
